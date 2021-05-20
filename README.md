c306f3393c0cd082584a8324c8f9265ec1c3de6d
Simone Scardapane <simonescardapane@gmail.com>
2021-05-20 16:33:28 +0200

    Added code

---
+++ README.md
@@ -0,0 +1,8 @@
+## Tutorial: Learning Hydra for configuring ML experiments
+
+[Blog post](https://sscardapane.it/tutorials/hydra-tutorial/) [Hydra documentation](https://hydra.cc/docs/next/tutorials/structured_config/schema)
+
+This is the companion code for my [blog post](https://sscardapane.it/tutorials/hydra-tutorial/) on using Hydra for configuring machine learning experiments.
+
+`main.py` provides the basic code for loading the configuration files inside the script and validating them using a configuration schema. `demo.ipynb` lists all the other examples described in the tutorial.
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Any
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+import os, logging
+logger = logging.getLogger(__name__)
+
+@dataclass
+class ImageConfig:
+    size: int
+    channels: int
+
+@dataclass
+class DatasetConfig:
+    image: ImageConfig
+    classes: int
+
+@dataclass
+class MainConfig:
+    dataset : DatasetConfig
+    feature_extractor: Any
+    classifier: Any
+
+cs = hydra.core.config_store.ConfigStore()
+cs.store(name="config_schema", node=MainConfig)
+
+@hydra.main(config_path='configs', config_name='config')
+def train(cfg: DictConfig):
+
+    # Print the configuration
+    print(OmegaConf.to_yaml(cfg))
+
+    # Log the current working directory
+    logger.info(f'Working dir: {os.getcwd()}')
+
+if __name__=='__main__':
+    train()
